ba2f7c95d6cc1389a90858c7bc6c84b9a87a90d4
Jordan Alba <jordan008@live.ca>
2021-08-27 09:43:04 -0400

    Initial Commit, example completed

--- FutureValue/Controllers/HomeController.cs
+++ FutureValue/Controllers/HomeController.cs
@@ -4,17 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using FutureValue.Models;
+
+
 namespace FutureValue.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.Name = "Mary";
-            ViewBag.FV = 99999.99;
+            ViewBag.FV = 0;
             return View();
         }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+
     }
 }
+
 
 
